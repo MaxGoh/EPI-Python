c2c75bc046d6eccc4e3856845ac7312fb4fb528e
Max Goh <themaxgohjh@gmail.com>
2018-11-04 15:30:35 +0800

    Chapter 5 - Arrays - 5.2 plus_one.py #3
    - INCREMENT AN ARBITRARY-PRECISION INTEGER

--- arrays/plus_one.py
+++ arrays/plus_one.py
@@ -2,6 +2,13 @@ from typing import List
 
 
 def plus_one(A: List[int]) -> List[int]:
+    """
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+
+    :param A: List[int
+    :return: List[int]
+    """
     A[-1] += 1
     for i in reversed(range(1, len(A))):
         if A[i] != 10:
